c520a73d1b90696fc0425c5eedaa066bf6edcfbc
opimandx <xwhiskasx@gmail.com>
2018-01-27 16:51:19 +0200

    It was awesome! :)

---
+++ app/src/main/java/com/londonappbrewery/bitcointicker/BitcoinDataModel.java
@@ -0,0 +1,28 @@
+package com.londonappbrewery.bitcointicker;
+
+import org.json.JSONObject;
+
+/**
+ * Created by opimand on 27.01.2018.
+ */
+
+public class BitcoinDataModel {
+    private String mPrice;
+
+    public String getPrice() {
+        return mPrice;
+    }
+
+    public static BitcoinDataModel fromJson(JSONObject jsonObject){
+        try {
+            BitcoinDataModel bitcoinData = new BitcoinDataModel();
+
+            bitcoinData.mPrice = jsonObject.getString("bid");
+            return bitcoinData;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+       return null;
+    }
+
+}
